0f8a04d2e7897ac820cc8f99e1f26c88e59b485f
Luis Angel <Luisparr14@gmail.com>
2021-05-26 20:30:15 -0500

    desplieue

--- config/database.js
+++ config/database.js
@@ -1,11 +1,5 @@
 var mysql = require('mysql')
-console.log('puerto en database: ',process.env.PORT); 
-var connection=mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
+var connection=mysql.createConnection(process.env.JAWSDB_URL)
 
 connection.connect((err)=>{
     if(err)throw err
