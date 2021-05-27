60926ca5e38c3f1e539db1f2321908a0369543f3
Luis Angel <Luisparr14@gmail.com>
2021-05-26 20:25:22 -0500

    desplieue

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,7 @@
 var app = require('../app')
 
 console.log(process.env.PORT);
-console.log(process.env.DB_HOSTNAME);
+console.log(process.env.JAWSDB_URL);
 app.listen(process.env.PORT || 8000, () => {
     console.log('conectado al puerto', process.env.PORT);
 })
