ff4404773675ec82bb959c32f531df77ab5c4d5c
Luis Angel <Luisparr14@gmail.com>
2021-05-26 20:14:57 -0500

    desplieue

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,7 @@
 var app=require('../app')
+
 console.log(process.env.PORT);
+
 app.listen(process.env.PORT||8000,()=>{
     console.log('conectado al puerto', process.env.PORT);
 })
--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 var connection=mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: '',
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 })
 
