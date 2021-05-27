4533a2f45b47ab57ea9533ce16a0eb27a6c1b5db
Luis Angel <Luisparr14@gmail.com>
2021-05-26 20:21:13 -0500

    desplieue

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,7 @@
-var app=require('../app')
+var app = require('../app')
 
 console.log(process.env.PORT);
 
-app.listen(process.env.PORT||8000,()=>{
+app.listen(process.env.PORT || 8000, () => {
     console.log('conectado al puerto', process.env.PORT);
 })
